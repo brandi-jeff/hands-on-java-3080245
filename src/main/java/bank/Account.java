0333d5346d94c68eb4c7c763312a7ba52cdd3019
brandi-jeff <112588194+brandi-jeff@users.noreply.github.com>
2022-11-11 21:49:06 +0000

    user engaement

--- src/main/java/bank/Account.java
+++ src/main/java/bank/Account.java
@@ -36,4 +36,12 @@ public class Account {
   public void setBalance(double balance) {
     this.balance = balance;
   }
+
+  public void deposit(double amount){
+
+  }
+
+  public void withdraw(double amount){
+    
+  }
 }
